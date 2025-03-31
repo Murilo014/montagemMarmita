efcc850fd3000aefaf3756cee7d49972d09d90b2
Murilo <murilonasoliveira@gmail.com>
2025-03-30 21:27:35 -0300

    Atualização feita com sucesso

--- main.py
+++ main.py
@@ -54,51 +54,49 @@ def cadastro_marmitas():
     porcoes = [("1", "Almoço"), ("2", "Janta")]
     return render_template("cadastroRefeicao.html", dias_da_semana=dias_da_semana, porcoes=porcoes)
 
+
 @app.route("/editaMarmita/<int:id_refeicao>", methods=["GET", "POST"])
 def edita_marmitas(id_refeicao):
-    
+    if request.method == 'POST':
+        dia = request.form.get("dia")
+        nome_refeicao = request.form.get("nome_refeicao")
+        ingredientes = request.form.get("ingredientes")
+        porcoes_selecionadas = request.form.getlist("porcoes")
+
+        dias_semana = {
+            '1': 'Segunda-feira',
+            '2': 'Terça-feira',
+            '3': 'Quarta-feira',
+            '4': 'Quinta-feira',
+            '5': 'Sexta-feira',
+            '6': 'Sábado',
+            '7': 'Domingo'
+        }
+        nome_dia = dias_semana.get(dia)
+
+        if atualizar_refeicao(id_refeicao, nome_dia, nome_refeicao, ingredientes, porcoes_selecionadas):
+            return redirect(url_for('marmitas'))
+        else:
+            return "Erro ao atualizar a refeição."
+
     if request.method == 'GET':
         refeicao, porcoes_associadas = obter_refeicao(id_refeicao)
 
         if refeicao is None:
             return "Refeição não encontrada", 404
         
-        dia = refeicao[1]
-        nome_refeicao = refeicao[2]
-        ingredientes = refeicao[3]
+        id_refeicao, dia, nome_refeicao, ingredientes = refeicao
 
         todas_porcoes = obter_porcoes()
         
         return render_template('editaMarmita.html',
-                               id_refeicao=refeicao[0],
+                               id_refeicao=id_refeicao,
                                dia=dia,
                                nome_refeicao=nome_refeicao,
                                ingredientes=ingredientes,
                                refeicao=refeicao,
                                porcoes_associadas=porcoes_associadas,
                                todas_porcoes=todas_porcoes)
-    
-    if request.method == 'POST':
-        dia = request.form.get("dia")
-        nome_refeicao = request.form.get("nome_refeicao")
-        ingredientes = request.form.get("ingredientes")
-
-        dias_semana = {
-            '1':'Segunda-feira',
-            '2':'Terça-feira',
-            '3':'Quarta-feira',
-            '4':'Quinta-feira',
-            '5':'Sexta-feira',
-            '6':'Sábado',
-            '7':'Domingo'
-        }
-        nome_dia = dias_semana.get(dia)
-
-        porcoes_selecionadas = request.form.getlist("porcoes")
-
-        atualizar_refeicao(id_refeicao, nome_dia, nome_refeicao, ingredientes, porcoes_selecionadas)
-
-        return redirect(url_for('marmitas'))
 
 if __name__ == "__main__":
     app.run(debug=True)
